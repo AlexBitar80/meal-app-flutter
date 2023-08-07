cc170035dc695daafc98fd70dbdd1a602b44e16c
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-08-07 13:27:23 -0300

    feat: adicionado componente de items de categoria para tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meals/pages/categories/categories_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,35 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  // ignore: library_private_types_in_public_api
-  _MyHomePageState createState() => _MyHomePageState();
-
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'DeliMeals',
-        ),
-      ),
-      body: const Center(
-        child: Text(
-          'Hello, Flutter!!',
-        ),
-      ),
+      home: const CategoriesPage(),
     );
   }
 }
