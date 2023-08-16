5c6c7edbdc94de106c6c59e597adc072762fe191
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-08-16 12:07:36 -0300

    feat: adicionado imagem da refeição e fazendo a exibição do seus ingredientes na tela de detalhes

--- lib/pages/meals/meal_detail_page.dart
+++ lib/pages/meals/meal_detail_page.dart
@@ -15,8 +15,50 @@ class MealDetailPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: const Center(
-        child: Text("Detalhes da refeição"),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 280,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            width: 300,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(meal.ingredients[index]),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
