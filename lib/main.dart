ca8f7d38f496950258e9edb8a3dc3411c2cfe92b
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-08-17 10:24:33 -0300

    feat: Adicionado barra de navegação na parte de baixo, configuração e troca para a tela de favoritos

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/pages/categories/categories_meals_page.dart';
 import 'package:meals/pages/categories/categories_page.dart';
 import 'package:meals/pages/meals/meal_detail_page.dart';
+import 'package:meals/pages/tabs/tabs_page.dart';
 import 'package:meals/utils/routes/app_routes.dart';
 
 void main() => runApp(const MyApp());
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: AppRoutes.home_page,
       routes: {
-        AppRoutes.home_page: (context) => const CategoriesPage(),
+        AppRoutes.home_page: (context) => const TabsPage(),
         AppRoutes.categories_meals: (context) => const CategoriesMealsPage(),
         AppRoutes.meal_detail: (context) => const MealDetailPage(),
       },
