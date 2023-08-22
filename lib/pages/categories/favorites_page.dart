8a7e2d536f9201c867db41dbc98281247072bbb1
João Alexandre Bitar <joaoalexandre.bitar@gmail.com>
2023-08-21 23:35:40 -0300

    feat: Adicionado funcionalidade para favoritar uma refeição e visualizar na tela de favoritos

--- lib/pages/categories/favorites_page.dart
+++ lib/pages/categories/favorites_page.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal_item.dart';
+
+import '../../models/meals.dart';
 
 class FavoritesPage extends StatelessWidget {
-  const FavoritesPage({super.key});
+  final List<Meal> favoriteMeals;
+
+  const FavoritesPage({
+    super.key,
+    required this.favoriteMeals,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Minhas refeições favoritas!!'),
-    );
+    if (favoriteMeals.isEmpty) {
+      return const Center(
+        child: Text('Nenhuma refeição foi marcada como favorita!'),
+      );
+    } else {
+      return ListView.builder(
+        itemCount: favoriteMeals.length,
+        itemBuilder: (context, index) {
+          return MealItem(meal: favoriteMeals[index]);
+        },
+      );
+    }
   }
 }
